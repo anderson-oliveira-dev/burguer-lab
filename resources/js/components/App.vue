07b185d46c682029693d5f11dc8e4276f83570a3
Anderson Santos <andersonmatheus011@hotmail.com>
2026-08-12 15:59:43 -0300

    Instala o bootstrap e monta o nav da aplicação

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,32 @@
 <template>
-    <div id="app">
-        <nav>
-            <RouterLink to="/">Início</RouterLink>
-            <RouterLink to="/orders">Pedidos</RouterLink>
-            <RouterLink to="/cart">Carrinho</RouterLink>
-            <RouterLink to="/profile">Perfil</RouterLink>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <RouterLink class="navbar-brand" to="/">BurguerLab</RouterLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" active-class="active" to="/">Início</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" active-class="active" to="/orders">Pedidos</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" active-class="active" to="/cart">Carrinho</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" active-class="active" to="/profile">Perfil</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
         </nav>
+    </div>
+
+    <div>
         <RouterView />
     </div>
 </template>
@@ -22,3 +43,10 @@ export default {
     }
 }
 </script>
+
+<style>
+.nav-link {
+    visibility: visible;
+}
+
+</style>
