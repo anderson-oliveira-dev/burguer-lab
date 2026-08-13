160cf0602c1fe325c989267867c0f10cdaf4e8ba
Anderson Santos <andersonmatheus011@hotmail.com>
2026-08-13 10:45:10 -0300

    Cria placeholder de produto

--- resources/js/components/home/Home.vue
+++ resources/js/components/home/Home.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>Lista de produtos</h1>
+    <div>
+        <h1>Cardápio</h1>
+        <div v-if="loading" class="row">
+            <div v-for="(item, index) in [1,2,3,4]" :key="index" class="col-3">
+                <ProductPlaceholder />
+            </div>
+        </div>
+        <div v-else>
+            Produtos
+        </div>
+    </div>
 </template>
+<script>
+import ProductPlaceholder from './placeholders/ProductPlaceholder.vue';
+
+export default{
+    components: {
+        ProductPlaceholder
+    },
+    data() {
+        return {
+            loading: true,
+        }
+    },
+    mounted(){
+        //loadProducts();
+    },
+    methods: {
+        loadProducts(){
+            setTimeout(() => {
+                this.loading = false;
+            }, 2000);
+        }
+    }
+}
+</script>
---
+++ resources/js/components/home/placeholders/ProductPlaceholder.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card" aria-hidden="true">
+        <div class="card-img-top bg-secondary" style="height: 200px;"></div>
+        <div class="card-body">
+            <h5 class="card-title placeholder-glow">
+                <span class="placeholder col-6"></span>
+            </h5>
+            <p class="card-text placeholder-glow">
+                <span class="placeholder col-7"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-4"></span>
+                <span class="placeholder col-6"></span>
+                <span class="placeholder col-8"></span>
+            </p>
+            <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+        </div>
+    </div>
+</template>
